1abd7369d8636b0e0ffe5338449e741499241596
JustinPaulLayan <justinpaul1025@gmail.com>
2022-01-24 21:31:45 +0800

    fixed duplicates

--- app/Http/Controllers/UserEmailController.php
+++ app/Http/Controllers/UserEmailController.php
@@ -17,28 +17,47 @@ class UserEmailController extends Controller
         foreach($namesDecode as $name) {
             foreach($numbersDecode as $number) {
                 if($name['email'] === $number['email']) {
-                    $isExist = false;
-                    for($ctr = 0; $ctr < count($datas); $ctr++) {
-                        if($datas[$ctr]['email'] === $number['email']) {
-                            unset($datas[$ctr]);
-                            $ctr = count($datas);
-                            $isExist = true;
-                        }
-                    }
-                    if(!$isExist) {
-                        $data = [
-                            "first_name" => $name['first_name'],
-                            "last_name" => $name['last_name'],
-                            "cc_number" => $number['cc_number'],
-                            "email" => $number['email']
-                        ];
-                        array_push($datas, $data);
-                    }
-                    break;
+                    $data = [
+                        "first_name" => $name['first_name'],
+                        "last_name" => $name['last_name'],
+                        "cc_number" => $number['cc_number'],
+                        "email" => $number['email']
+                    ];
+                    array_push($datas, $data);
                 }
             }
         }
 
+        $datas = $this->findDuplicate($datas);
+
         return view('welcome', ['datas' => $datas]);
+    }
+
+    private function findDuplicate($datas) {
+        $tempArray = array();
+        $i = 0;
+        $keyArray = array();
+        $returnArray = array();
+        $emailNeedToDeleted = array();
+    
+        foreach($datas as $val) {
+            if (!in_array($val['email'], $keyArray)) {
+                $keyArray[$i] = $val['email'];
+                $tempArray[$i] = $val;
+            } else {
+                $emailNeedToDeleted[$i] = $val['email'];
+            }
+            $i++;
+        }
+
+        $i = 0;
+        foreach($datas as $val) {
+            if (!in_array($val['email'], $emailNeedToDeleted)) {
+                $returnArray[$i] = $val;
+            }
+            $i++;
+        }
+
+        return $returnArray;
     }
 }
