51001802b6cf0677896336bbca4596da5be8e31f
JustinPaulLayan <justinpaul1025@gmail.com>
2022-01-24 20:00:17 +0800

    update

--- app/Http/Controllers/UserEmailController.php
+++ app/Http/Controllers/UserEmailController.php
@@ -17,13 +17,23 @@ class UserEmailController extends Controller
         foreach($namesDecode as $name) {
             foreach($numbersDecode as $number) {
                 if($name['email'] === $number['email']) {
-                    $data = [
-                        "first_name" => $name['first_name'],
-                        "last_name" => $name['last_name'],
-                        "cc_number" => $number['cc_number'],
-                        "email" => $number['email']
-                    ];
-                   array_push($datas, $data);
+                    $isExist = false;
+                    for($ctr = 0; $ctr < count($datas); $ctr++) {
+                        if($datas[$ctr]['email'] === $number['email']) {
+                            unset($datas[$ctr]);
+                            $ctr = count($datas);
+                            $isExist = true;
+                        }
+                    }
+                    if(!$isExist) {
+                        $data = [
+                            "first_name" => $name['first_name'],
+                            "last_name" => $name['last_name'],
+                            "cc_number" => $number['cc_number'],
+                            "email" => $number['email']
+                        ];
+                        array_push($datas, $data);
+                    }
                     break;
                 }
             }
